24bf2ea83cacd6347b00321a361644613d23d1d9
yDewolf <andrersng@gmail.com>
2026-06-05 14:59:15 -0300

    LeftTab Dropdown (WIP)

--- src/editor/ui/panels/base-panels.tsx
+++ src/editor/ui/panels/base-panels.tsx
@@ -72,7 +72,11 @@ export const Dropdown = (props: {content: JSXElement, animated?: boolean}) => {
         </Show>
     )
     return (
-        <div class="dropdown" classList={{"expanded": expanded(), "no-animation": !props.animated}}>
+        <div 
+            class="dropdown" 
+            classList={{"expanded": expanded(), "no-animation": !props.animated}}
+            onFocusOut={() => setExpanded(false)}
+        >
             <div class="dropdown-header" classList={{"expanded": expanded()}}>
                 <button class="icon-button section-dropdown-icon" onclick={() => {
                     if (expanded()) setExpanded(false); else setExpanded(true);
@@ -90,6 +94,111 @@ export const Dropdown = (props: {content: JSXElement, animated?: boolean}) => {
         </div>
     )
 }
+
+export const DropdownItemButton = (props: {label: string, onClick: () => void, onMouseOver?: () => void}) => {
+    return (
+        <button
+            onclick={() => {props.onClick()}}
+            onMouseOver={() => {props.onMouseOver?.()}}
+            class="dropdown-item dropdown-button dropdown-label space-between"
+        >
+            {props.label}
+        </button>
+    )
+}
+
+export const Dropside = (props: {
+    label_element?: JSXElement,
+    label?: string,
+    content: JSXElement,
+    animated?: boolean,
+    visible?: () => boolean,
+    onMouseEnter?: () => void
+}) => {
+    const [localVisible, setLocalVisible] = createSignal(true);
+    const is_visible = props.visible ?? localVisible;
+
+    const renderContent = () => (
+        <Show when={is_visible()}>
+            <div 
+                class="dropside-wrapper"
+                onMouseLeave={() => setLocalVisible(false)}
+            >
+                <div class="dropside-content">
+                    {props.content}
+                </div>
+            </div>
+        </Show>
+    );
+
+    return (
+        <div 
+            class="dropdown-item"
+            classList={{
+                "expanded": is_visible()
+            }}
+            onMouseEnter={() => {
+                if (props.onMouseEnter) {
+                    props.onMouseEnter();
+                    return;
+                }
+                setLocalVisible(true)
+            }}
+        >
+            <div class="dropdown-label space-between">
+                <Show when={props.label_element} fallback={
+                    <button class="dropdown-button">
+                        {props.label}
+                    </button>
+                }>
+                    {props.label_element}
+                </Show>
+                <ArrowDownIcon class="dropside-icon"/>
+            </div>
+            <Show when={props.animated} fallback={renderContent()}>
+                <Transition name="dropside-fade">
+                    {renderContent()}
+                </Transition>
+            </Show>
+        </div>
+    );
+}
+
+export interface DropsideItemData {
+    label_element?: () => JSXElement,
+    label?: string,
+    content: () => JSXElement,
+    animated?: boolean
+}
+
+export const DropsideManager = (props: {
+    dropsides: Map<string, DropsideItemData>,
+    currentItem?: () => string | undefined,
+    setCurrentItem?: (item: string | undefined) => void,
+}) => {
+    const [currentItem, setCurrentItem] = createSignal<string | undefined>(undefined);
+    const current_item = props.currentItem ?? currentItem;
+    const set_current_item = props.setCurrentItem ?? setCurrentItem;
+
+    return (
+        <div class="dropside-manager-container">
+            <For each={Array.from(props.dropsides.entries())}>
+                {([id, data]) => {
+                    return (
+                        <Dropside 
+                            label_element={data.label_element?.()}
+                            label={data.label}
+                            content={data.content()}
+                            animated={data.animated}
+                            visible={() => current_item() === id}
+                            onMouseEnter={() => set_current_item(id)}
+                        />
+                    );
+                }}
+            </For>
+        </div>
+    );
+};
 
 
 export const TabDisplayer = (props: {
--- src/editor/ui/panels/left-tab.tsx
+++ src/editor/ui/panels/left-tab.tsx
@@ -2,12 +2,38 @@ import { Accessor, createRoot, createSignal, JSXElement, onCleanup, Show } from 
 import { ToolController } from "~/editor/controllers/tool-controller";
 import { SceneController } from "~/wrapper/controllers/scene-controller";
 import { NodeTypePreview, NodeTypeSelector } from "./type_selector/node-type-selector";
-import { Dropdown, DropdownSection, TabSelector } from './base-panels';
+import { Dropdown, DropdownSection, Dropside, DropsideItemData, DropsideManager, TabSelector, DropdownItemButton } from './base-panels';
 import { session_controller } from "~/singletons/user_session";
 import { FileExplorer } from "./file/file-explorer";
 import LeftTabIcon from "~/assets/icons/left-tab.svg";
 
-export const EditorLeftTabHolder = (props: {node_type_selector: NodeTypeSelector, scene_controller: SceneController, tool_controller: ToolController}) => {
+const LeftTabDropdown = (props: {}) => {
+    const [currentItem, setCurrentItem] = createSignal<string | undefined>(undefined);
+    const subsections: Map<string, DropsideItemData> = new Map([
+        ["file", {
+            label: "File",
+            content: () => <div class="dropdown-item-grid">
+                <button onclick={() => {}} class="dropdown-button">Save Scene</button>
+                <button onclick={() => {}} class="dropdown-button">Load Scene</button>
+            </div>
+        }],
+    ])
+    return (
+        <Dropdown content={
+            <div
+                class="fill dropdown-item-grid">
+                <DropsideManager dropsides={subsections} currentItem={currentItem} setCurrentItem={setCurrentItem}/>
+                <DropdownItemButton label="Settings" onClick={() => {}} onMouseOver={setCurrentItem(undefined)}/>
+            </div>
+        }/>
+    )
+}
+
+export const EditorLeftTabHolder = (props: {
+    node_type_selector: NodeTypeSelector, 
+    scene_controller: SceneController, 
+    tool_controller: ToolController
+}) => {
     const tabs: Record<string, () => JSXElement> = {
         "Nodes": () => props.node_type_selector.View(props.scene_controller, (node_preview: NodeTypePreview) => props.tool_controller.current_tool?.onClickOnNodePreview(node_preview)),
         "Workspace": () => <FileExplorer workspace={session_controller.user_workspace}/>
@@ -27,13 +53,7 @@ export const EditorLeftTabHolder = (props: {node_type_selector: NodeTypeSelector
                             <span class="icon-span">
                                 <img src="public/assets/logo/placeholder-logo.png" alt="Nodeblocks" />
                             </span>
-                            <Dropdown content={
-                                <div class="container">
-                                    <button class="">TODO</button>
-                                    <button class="">TODO</button>
-                                    <button class="">TODO</button>
-                                </div>
-                            }/>
+                            <LeftTabDropdown />
                         </div>
                         <h4>
                             NodeScene
